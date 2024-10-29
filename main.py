7124db493d1e1b4360be2ca9c9a1a74778f7e494
RCubed <xertremble@gmail.com>
2024-10-29 15:55:14 +0800

    folders made

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, './Classes/')
+sys.path.insert(0, './Enemies/')
+sys.path.insert(0, './Equipments/')
+sys.path.insert(0, './Items/')
+sys.path.insert(0, './Saves/')
+sys.path.insert(0, './Questions/')
+
+import classes.py
+
+def main():
+    pass
+
+main()
